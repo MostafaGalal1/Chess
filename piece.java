0b40c936691e102588ccd8f3d043420a61301fc3
michaelmonir <36045756+michaelmonir@users.noreply.github.com>
2022-09-16 23:27:32 +0200

    Update piece.java

--- piece.java
+++ piece.java
@@ -1,38 +1,81 @@
 import java.awt.*;
+import java.util.ArrayList;
 import java.util.List;
 
-public  class piece extends square
+public class bishop extends piece
 {
-    public piece(int xx, int yy) {
-        this.x = xx;
-        this.y = yy;
+    public bishop(int x, int y)
+    {
+        super(x, y);
     }
 
+    public bishop(int xx, int yy, color c){super(xx, yy, c);}
+
+    @Override
     public List<Point> getvalid(int x1, int y1)
     {
-        return null;
+        List<Point> list = new ArrayList<>();
+
+        for (int i = 1; i <= 8; i++)
+        {
+            int x2 = x1 + i, y2 = y1 + i;
+            if (!square.isvalid_square(x2, y2)) break;
+
+            if (board.chess[x2][y2].ispieace())
+            {
+                if (((piece) board.chess[x2][y2]).piece_color != this.piece_color)
+                    list.add(new Point(x2, y2));
+                break;
+            }
+
+            list.add(new Point(x2, y2));
+        }
+
+        for (int i = 1; i <= 8; i++)
+        {
+            int x2 = x1 + i, y2 = y1 - i;
+            if (!square.isvalid_square(x2, y2)) break;
+
+            if (board.chess[x2][y2].ispieace())
+            {
+                if (((piece) board.chess[x2][y2]).piece_color != this.piece_color)
+                    list.add(new Point(x2, y2));
+                break;
+            }
+
+            list.add(new Point(x2, y2));
+        }
+
+        for (int i = 1; i <= 8; i++)
+        {
+            int x2 = x1 - i, y2 = y1 + i;
+            if (!square.isvalid_square(x2, y2)) break;
+
+            if (board.chess[x2][y2].ispieace())
+            {
+                if (((piece) board.chess[x2][y2]).piece_color != this.piece_color)
+                    list.add(new Point(x2, y2));
+                break;
+            }
+
+            list.add(new Point(x2, y2));
+        }
+
+        for (int i = 1; i <= 8; i++)
+        {
+            int x2 = x1 - i, y2 = y1 - i;
+            if (!square.isvalid_square(x2, y2)) break;
+
+            if (board.chess[x2][y2].ispieace())
+            {
+                if (((piece) board.chess[x2][y2]).piece_color != this.piece_color)
+                    list.add(new Point(x2, y2));
+                break;
+            }
+
+            list.add(new Point(x2, y2));
+        }
+
+        return list;
     }
-
-
-    public boolean ispiece()
-    {
-        return true;
-    }
-
-    // public color getcolor     return this.color
-
-    public void move()
-    {
-        System.out.println("move piece");
-    }
-
-
 }
-
-enum color
-{
-    white, black
-}
-
-
-
