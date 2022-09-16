2bcdee1d38489c020ccfcdbb22d12015116063b1
michaelmonir <36045756+michaelmonir@users.noreply.github.com>
2022-09-16 23:06:38 +0200

    Update bishop.java

--- bishop.java
+++ bishop.java
@@ -1,9 +1,31 @@
+import java.awt.*;
+import java.util.ArrayList;
+import java.util.List;
 
-
-public class bishop
+public class bishop extends piece
 {
-    public void isvalid()
+    public bishop(int x, int y)
     {
+        super(x, y);
+    }
 
+    @Override
+    public List<Point> getvalid(int x1, int y1)
+    {
+        List<Point> list = new ArrayList<>();
+        for (int i = -8; i <= 8; i++)
+        {
+            int x2 = x1 + i, y2 = y1 + i;
+            if (x2 <= 7 && x2 >= 0 && y1 <= 7 && y1 >= 0 )
+                list.add(new Point(x2, y2));
+        }
+        for (int i = -8; i <= 8; i++)
+        {
+            int x2 = x1 + i, y2 = y1 - i;
+            if (x2 <= 7 && x2 >= 0 && y1 <= 7 && y1 >= 0 )
+                list.add(new Point(x2, y2));
+        }
+
+        return list;
     }
 }
