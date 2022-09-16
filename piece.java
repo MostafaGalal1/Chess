472f8720f98a76db6ae91a7b40bd9fcd7bd725fd
michaelmonir <36045756+michaelmonir@users.noreply.github.com>
2022-09-16 23:07:40 +0200

    Update piece.java

--- piece.java
+++ piece.java
@@ -1,9 +1,18 @@
+import java.awt.*;
+import java.util.List;
+
 public  class piece extends square
 {
     public piece(int xx, int yy) {
         this.x = xx;
         this.y = yy;
     }
+
+    public List<Point> getvalid(int x1, int y1)
+    {
+        return null;
+    }
+
 
     public boolean ispiece()
     {
